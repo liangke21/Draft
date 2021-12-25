700a6cdbd9f1c39159565552980e93e16d95c649
liangke21 <l1396797522@foxmail.com>
2021-12-25 21:02:52 +0800

    添加MyNavigation

---
+++ Android/MyNavigation/app/src/main/java/com/example/mynavigation/MainActivity.kt
@@ -0,0 +1,118 @@
+package com.example.mynavigation
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mynavigation.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarMain.toolbar)
+
+        binding.appBarMain.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+            binding.drawerLayout.closeDrawer(Gravity.RIGHT)//点击关闭右侧抽屉
+            binding.drawerLayout.openDrawer(binding.navView)//关闭右侧抽屉的同时在打开左侧抽屉
+        }
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+
+
+
+       appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        navView.setupWithNavController(navController)
+
+/*        navView.setNavigationItemSelectedListener(object: NavigationView.OnNavigationItemSelectedListener {
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                Log.d("NavigationItem", item.groupId.toString() + " title " + item.title+" itemId "+item.itemId+" groupId"+item.groupId)
+                binding.drawerLayout.openDrawer(Gravity.RIGHT)
+                binding.drawerLayout.closeDrawer(binding.navView)
+                return true
+            }
+        })*/
+
+         val navViewView= navView.getHeaderView(0)
+        navViewView.findViewById<Button>(R.id.nav_gallery).setOnClickListener {
+            binding.drawerLayout.openDrawer(Gravity.RIGHT)
+            binding.drawerLayout.closeDrawer(binding.navView)
+            Log.d("NavigationItem"," ")
+        }
+
+        drawer()
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    @SuppressLint("WrongConstant", "RtlHardcoded")
+    fun drawer(){
+
+
+        binding.drawerLayout.openDrawer(binding.navView)
+        //左右侧导航栏
+
+        binding.drawerLayout.addDrawerListener(object : DrawerLayout.DrawerListener {
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+
+            }
+
+            override fun onDrawerOpened(drawerView: View) {
+
+            }
+
+            override fun onDrawerClosed(drawerView: View) {
+
+            }
+
+            override fun onDrawerStateChanged(newState: Int) {
+
+
+            }
+        })
+    }
+
+}
